cddc9abcd1b2303aeaf3f05f08184a958c8464fe
Chaitrali Naidu <naiduchaitrali2807@gmail.com>
2024-01-10 13:25:06 +0530

    final

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:[
+            "vvsinoxrghvvpvpijgoa.supabase.co"
+        ]
+    }
+}
+
+module.exports = nextConfig
